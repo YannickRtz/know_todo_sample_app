a872e45777c7762996029c482d6cf306e1c3ed9d
Yannick Rietz <rietz@projektionisten.de>
2023-06-04 18:43:41 +0200

    feat: refactor todolist widget

--- lib/features/todos/ui/todo_list.dart
+++ lib/features/todos/ui/todo_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:knowunity_todo/features/todos/domain/controller/todos_controller.dart';
+import 'package:knowunity_todo/features/todos/domain/models/todo.dart';
 
 class TodoList extends ConsumerWidget {
   const TodoList({Key? key}) : super(key: key);
@@ -12,18 +13,22 @@ class TodoList extends ConsumerWidget {
       data: (todos) {
         return ListView(
           children: todos.map((t) {
-            return ListTile(
-              leading: Checkbox(
-                value: t.completed,
-                onChanged: (_) => ref.read(todosProvider.notifier).toggle(t),
-              ),
-              title: Text(t.title),
-            );
+            return _buildTodo(t, ref);
           }).toList(),
         );
       },
       error: (e, _) => Text(e.toString()),
       loading: () => const Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  ListTile _buildTodo(Todo t, WidgetRef ref) {
+    return ListTile(
+      leading: Checkbox(
+        value: t.completed,
+        onChanged: (_) => ref.read(todosProvider.notifier).toggle(t),
+      ),
+      title: Text(t.title),
     );
   }
 }
